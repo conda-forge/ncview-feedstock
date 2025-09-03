c351efda9bba32c566240ca0c776086b65a2953f
Xylar Asay-Davis <xylarstorm@gmail.com>
2025-09-03 17:07:33 +0200

    Update configure flags

--- recipe/build.sh
+++ recipe/build.sh
@@ -41,14 +41,14 @@ fi
 
 export ac_cv_lib_expat_XML_GetBase=yes
 
-./configure --prefix=${PREFIX} \
-            --x-libraries=${X_PREFIX}/lib \
-            --x-includes=${X_PREFIX}/include \
-            --with-nc-config=${PREFIX}/bin/nc-config \
-            --with-udunits2_incdir=${PREFIX}/include \
-            --with-udunits2_libdir=${PREFIX}/lib \
-            --with-png_incdir=${PREFIX}/include \
-            --with-png_libdir=${PREFIX}/lib
+./configure --prefix="${PREFIX}" \
+            --x-includes="${PREFIX}/include" \
+            --x-libraries="${PREFIX}/lib" \
+            --with-nc-config="${PREFIX}/bin/nc-config" \
+            --with-udunits2_incdir="${PREFIX}/include" \
+            --with-udunits2_libdir="${PREFIX}/lib" \
+            --with-png_incdir="${PREFIX}/include" \
+            --with-png_libdir="${PREFIX}/lib"
 
 make
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
